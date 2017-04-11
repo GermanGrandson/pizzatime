8bebf85639b771e47ff85cf3697a6c4a7dd0c346
German Grandson <ganieto1@gmail.com>
2017-04-11 07:46:36 -0400

    config geocoder

--- app/services/google_places.rb
+++ app/services/google_places.rb
@@ -3,13 +3,13 @@ require 'json'
 require 'pry'
 
 # Key is hid with ENV variables using Figaro
-
+API_KEY = "AIzaSyBITsYDCU8mfLW9exGEBrU6vy5xTNyV264"
 class GooglePlaces
 
   # Lat and Lng are passed in using Geocoder gem. Variables passes into google places api
   # in order to find pizza places nearby.
   def find_places(lat, lng)
-    response = HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{lat},#{lng}&radius=300&type=restaurant&keyword=pizza&rankBy=distance&key=#{ENV['google_key']}")
+    response = HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{lat},#{lng}&radius=300&type=restaurant&keyword=pizza&rankBy=distance&key=#{API_KEY}")
     sleep(1)
     data = JSON.parse(response.body)['results']
     name = []
@@ -22,7 +22,7 @@ class GooglePlaces
   # Lat and Lng are found using google geocde api. We pass in address scraped from Yelp
   # then the lat lng are passed into google maps api to display map location of pizza shop.
   def geocode(address)
-    response = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{address.gsub(' ','+')}&key=#{ENV['google_key']}")
+    response = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{address.gsub(' ','+')}&key=#{API_KEY}")
     sleep(1)
     data = JSON.parse(response.body)['results']
     location = []
