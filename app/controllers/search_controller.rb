f087a1e111f38a145413b40b177ef35a1c76ea71
German Grandson <ganieto1@gmail.com>
2017-04-19 15:02:10 -0400

    refactor with 2 models

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -10,19 +10,12 @@ class SearchController < ApplicationController
   # from Scrape.scrape_shop and will be redirect back to path with flash notice.
   def results
     @pizza_shop = params['pizza']['shop']
-    @limit = params['pizza']['limit'].to_i - 1
-    @scrape = Scrape.new.scrape_shop(@pizza_shop, @limit)
-    if @scrape == 'No Result'
-      redirect_to root_path, notice: 'No results found. Please try again.'
+    @limit = params['pizza']['limit'].to_i
+    if @scrape = Scrape.new.scrape_shop(@pizza_shop, @limit)
+      @location = GooglePlaces.new.geocode(@scrape.first.address)
     else
-      @scrape.each do |shop_hash|
-        @shop = shop_hash[:shop]
-        @address = shop_hash[:address]
-        @phone = shop_hash[:phone]
-        @time = shop_hash[:time]
-      end
-      @location = GooglePlaces.new.geocode(@address)
+      redirect_to root_path, notice: 'No results found. Please try again.'
     end
   end
-  
+
 end
--- app/services/scrape.rb
+++ app/services/scrape.rb
@@ -6,43 +6,46 @@ class Scrape
   # restaurant link and scrape its data. Data gets pushed into array of hashes.
   def scrape_shop(pizza_shop, limit)
     mechanize = Mechanize.new
-    sleep(2) #Used to avoid timeout issues with Mechanize
+    # sleep(2) #Used to avoid timeout issues with Mechanize
     begin
       page = mechanize.get("https://www.yelp.com/search?find_desc=#{pizza_shop}+pizza&find_loc=New+York,+NY")
+
+      if page.root.css('.search-header-title-container > h1').text.strip.include? "No Results"
+        false
+      else
+        link = page.root.css('.regular-search-result > .search-result > .biz-listing-large > .main-attributes > .media-block > .media-story > .search-result-title > .indexed-biz-name > a').first
+        link = link.attributes['href'].value
+
+        review_page = mechanize.get("http://yelp.com#{link}?sort_by=date_desc")
+        yelp_reviews = review_page.root.css('.review')
+        reviews_array = []
+
+        yelp_reviews[1..limit].each do |yelp_review|
+          rating = yelp_review.css('.review-wrapper > .review-content > .biz-rating > div > .i-stars').attribute('title').value
+          date = yelp_review.css('.review-wrapper > .review-content > .biz-rating > .rating-qualifier').children.text.strip
+          text = yelp_review.css('.review-wrapper > .review-content > p').text.strip
+          pic = yelp_review.css('.review-sidebar > .review-sidebar-content > .ypassport > .media-avatar > .photo-box > a > img').attribute('src').value
+          username = yelp_review.css('.review-sidebar > .review-sidebar-content > .ypassport > .media-story > .user-passport-info > .user-name > a').text.strip
+
+          review = Review.new(rating, date, text, pic, username)
+          reviews_array << review
+        end
+
+        name = review_page.root.css('.biz-page-title').text.strip
+        address = review_page.root.css('.street-address').text.strip
+        phone = review_page.root.css('.biz-phone').text.strip
+        time = review_page.root.css('.u-space-r-half').text.strip
+
+        shop = Shop.new(name, address, phone, time)
+        return shop, reviews_array
+      end
+
     rescue Net::HTTPInternalError
       puts 'An error occurred'
     rescue ResponseCodeError
       puts "ResponseCodeError occurred"
     end
-    sleep(2)
-    if page.root.css('.search-header-title-container > h1').text.strip.include? "No Results"
-      "No Result"
-    else
-      link = page.root.css('.regular-search-result > .search-result > .biz-listing-large > .main-attributes > .media-block > .media-story > .search-result-title > .indexed-biz-name > a').first
-      link = link.attributes['href'].value
 
-      review_page = mechanize.get("http://yelp.com#{link}?sort_by=date_desc")
-      yelp_reviews = review_page.root.css('.review')
-      reviews_array = []
-
-      yelp_reviews[1..10].each do |yelp_review|
-        sleep(1) if limit > 5 #Used to avoid timeout issues with Mechanize
-        review_values = {}
-        review_values[:rating] = yelp_review.css('.review-wrapper > .review-content > .biz-rating > div > .i-stars').attribute('title').value
-        review_values[:date] = yelp_review.css('.review-wrapper > .review-content > .biz-rating > .rating-qualifier').children.text.strip
-        review_values[:text] = yelp_review.css('.review-wrapper > .review-content > p').text.strip
-        review_values[:pic] = yelp_review.css('.review-sidebar > .review-sidebar-content > .ypassport > .media-avatar > .photo-box > a > img').attribute('src').value
-        review_values[:name] = yelp_review.css('.review-sidebar > .review-sidebar-content > .ypassport > .media-story > .user-passport-info > .user-name > a').text.strip
-        reviews_array << review_values
-      end
-
-      review_values = {}
-      review_values[:shop] = review_page.root.css('.biz-page-title').text.strip
-      review_values[:address] = review_page.root.css('.street-address').text
-      review_values[:phone] = review_page.root.css('.biz-phone').text.strip
-      review_values[:time] = review_page.root.css('.u-space-r-half').text.strip
-      reviews_array.push(review_values)
-    end
   end
 
 end
